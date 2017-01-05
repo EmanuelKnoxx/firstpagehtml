adb4f7a9970bf88bae7924e2d803d1544d425c49
emanuelknoxx <emanuelknoxx@gmail.com>
2017-01-05 06:44:55 -0300

    test console log

---
+++ app.js
@@ -0,0 +1,12 @@
+/*console.log("nano vou agora ja foi!");
+console.log("e fudeu");*/ 
+var nome="Emanuel";
+console.log(nome);
+var sobrenome="Elias";
+console.log(sobrenome);
+var nomecompleto=nome + " " + sobrenome;
+console.log(nomecompleto);
+var num1=42;
+var num2=21;
+var soma=num1+num2;
+console.log("a soma total é", soma+num1+num2);
